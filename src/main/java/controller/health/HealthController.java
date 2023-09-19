b2950203ed84c435cbfe349a7b7781a4b7ce4202
rayo100 <cv.1000458640@gmail.com>
2023-09-18 23:49:00 -0500

    Finalizado

--- src/main/java/controller/health/HealthController.java
+++ src/main/java/controller/health/HealthController.java
@@ -1,10 +1,11 @@
 package controller.health;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping("/health")
 public class HealthController {
-    @RequestMapping("/health")
+    @GetMapping
     public String checkAPI(){
        return "<h1>The API is working ok!</h1>";
     }
